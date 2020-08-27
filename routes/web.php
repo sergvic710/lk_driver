da8b7d89b9b38dfd0fde3b5513bbd369b3c7fae0
sergei <sergvic@yandex.ru>
2020-08-27 18:05:05 +0300

    change link to Docs

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/docs', 'DocsController@index');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
