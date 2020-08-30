7e1ab80fb31e68780d3a398c60c14e7cf7a17a19
sergei <sergvic@yandex.ru>
2020-08-30 14:24:47 +0300

    change password

--- resources/views/changePassword.blade.php
+++ resources/views/changePassword.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-10">
+            <div class="col-md-10hf ghj">
                 <div class="card">
                     <div class="card-header">Изменение пароля</div>
                     <div class="card-body">
