3d8ea749dd7d0e41eb5223e7e81e80880b0c9b50
sergei <sergvic@yandex.ru>
2020-09-06 16:51:23 +0300

    add force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if(env('FORCE_HTTPS',false)) {
+            URL::forceScheme('https');
+        }
+
         Builder::defaultStringLength(191); // Update defaultStringLength
     }
 }
